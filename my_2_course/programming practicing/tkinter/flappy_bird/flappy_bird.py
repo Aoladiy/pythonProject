165dd3577a07a0cafb4dea0d8c2cb20d8074914e
Aldar <ochirov-3@mail.ru>
2023-04-03 20:13:38 +0300

    hopefully final version of flappy bird

--- my_2_course/programming practicing/tkinter/flappy_bird/flappy_bird.py
+++ my_2_course/programming practicing/tkinter/flappy_bird/flappy_bird.py
@@ -1,86 +1,79 @@
 import random
 import tkinter as tk
 
-# Создаем главное окно
+# Скорость птицы
+bird_speed = 0
+# Плотность труб от 50 до 400
+pipe_density = 200
+# Размер окна
+window_size = 400
+
 root = tk.Tk()
-
-# Задаем размер окна
-root.geometry("400x400")
-
-# Добавляем название игры
+root.geometry(f"{window_size}x{window_size}")
 root.title("Flappy Bird")
 
-# Создаем фоновый цвет
-canvas = tk.Canvas(root, width=400, height=400, bg="sky blue")
+canvas = tk.Canvas(root, width=window_size, height=window_size, bg="sky blue")
 canvas.pack()
 
-# Создаем птицу
-bird = canvas.create_oval(50, 200, 70, 220, fill="white")
 
-# Задаем скорость птицы
-bird_speed = 0
-
-
-# Генерируем препятствия
 class Obstacle:
     def __init__(self):
         self.color = "green"
+        hole_location = random.randint(100, window_size - 100)
+        hole_size = 50
 
-        # Создаем верхнее препятствие
-        top_height = random.randint(50, 200)
-        self.top = canvas.create_rectangle(400, 0, 350, top_height, fill=self.color)
+        self.top = canvas.create_rectangle(window_size + 50, 0, window_size, hole_location - hole_size, fill=self.color)
 
-        # Создаем нижнее препятствие
-        bottom_height = random.randint(50, 200)
-        self.bottom = canvas.create_rectangle(400, 400, 350, 400 - bottom_height, fill=self.color)
+        self.bottom = canvas.create_rectangle(window_size + 50, window_size, window_size, hole_location + hole_size,
+                                              fill=self.color)
 
-    def collision(self):
+    def in_pipe(self):
         canvas.itemconfig(self.top, fill="red")
         canvas.itemconfig(self.bottom, fill="red")
 
+    def out_pipe(self):
+        canvas.itemconfig(self.top, fill="green")
+        canvas.itemconfig(self.bottom, fill="green")
 
-# Обработка нажатия клавиши
+
 def on_key_press(event):
     global bird_speed
     bird_speed = -10
 
 
-# Функция движения птицы и препятствий
-def move(obstacle):
+canvas.bind_all('<space>', on_key_press)
+
+
+def move(obstacles):
     global bird_speed
-    # Двигаем птицу
-    if max(canvas.coords(bird)[1], canvas.coords(bird)[3]) <= 400 or bird_speed < 0:
+
+    if max(canvas.coords(bird)[1], canvas.coords(bird)[3]) <= window_size or bird_speed < 0:
         canvas.move(bird, 0, bird_speed)
         bird_speed += 1
 
-    # Двигаем препятствия
-    canvas.move(obstacle.top, -5, 0)
-    canvas.move(obstacle.bottom, -5, 0)
+    for obstacle in obstacles:
+        canvas.move(obstacle.top, -5, 0)
+        canvas.move(obstacle.bottom, -5, 0)
 
-    # Проверяем столкновение с препятствиями
-    if canvas.bbox(bird)[0] < canvas.bbox(obstacle.top)[2] \
-            and canvas.bbox(bird)[2] > canvas.bbox(obstacle.top)[0] \
-            and (canvas.bbox(bird)[1] < canvas.bbox(obstacle.top)[3]
-                 or canvas.bbox(bird)[3] > canvas.bbox(obstacle.bottom)[1]):
-        obstacle.collision()
+        if canvas.bbox(bird)[0] < canvas.bbox(obstacle.top)[2] \
+                and canvas.bbox(bird)[2] > canvas.bbox(obstacle.top)[0] \
+                and (canvas.bbox(bird)[1] < canvas.bbox(obstacle.top)[3]
+                     or canvas.bbox(bird)[3] > canvas.bbox(obstacle.bottom)[1]):
+            obstacle.in_pipe()
+        else:
+            obstacle.out_pipe()
 
-    # Генерируем новые препятствия при необходимости
     if canvas.bbox(obstacle.top)[2] < 0:
         canvas.delete(obstacle.top)
         canvas.delete(obstacle.bottom)
-        obstacle = Obstacle()
+    if canvas.bbox(obstacle.top)[2] < pipe_density:
+        obstacles.append(Obstacle())
 
-    # Запускаем функцию снова через 25 миллисекунд
-    canvas.after(25, move, obstacle)
+    canvas.after(25, move, obstacles)
 
 
-# Привязываем обработчик нажатия клавиши к событию нажатия клавиши
-canvas.bind_all('<space>', on_key_press)
+bird = canvas.create_oval(50, 200, 70, 220, fill="white")
+obstacles = [Obstacle()]
+move(obstacles)
 
-# Запускаем игру
-# Задаем начальное препятствие
-obstacle = Obstacle()
-move(obstacle)
-
-# Запускаем главный цикл обработки событий
 root.mainloop()
